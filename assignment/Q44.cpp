7eeb71f2bc0adfd374aa4f276f4e92edd3e563ff
Aayush Chopra <aayushking37@gmail.com>
2023-10-11 00:00:24 +0530

    commit

--- assignment/Q44.cpp
+++ assignment/Q44.cpp
@@ -1,2 +1,35 @@
 #include<iostream>
 using namespace std;
+class person{
+    protected:
+    char name[100];
+    int age;
+
+    public:
+    void person_detail(){
+        cout<<"Name: ";
+        gets(name);
+        cout<<"Age: ";
+        cin>>age;
+    }
+};
+
+class student:public person{
+    protected:
+    int stu_id;
+
+    public:
+    void get_id(){
+        cout<<"enter student id: ";
+        cin>>stu_id;
+    }
+    void show_data(){
+        cout<<"Student Name: "<<name<<endl;
+        cout<<"Student Age: "<<age<<endl;
+        cout<<"Student ID: "<<stu_id<<endl;
+    }
+};
+
+class exam:public student{
+    
+};
