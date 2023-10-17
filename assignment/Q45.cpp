655c8e555d6d12295d63ef0d046aee537369b048
Aayush Chopra <aayushking37@gmail.com>
2023-10-17 23:55:57 +0530

    commit

--- assignment/Q45.cpp
+++ assignment/Q45.cpp
@@ -1,62 +1,127 @@
+//Q45
 #include<iostream>
 using namespace std;
 class publication{
     protected:
-    string title;
-    float price;
+        char title[30];
+        float price;
 
     public:
-    void getdata(){
-        cout<<"enter title: ";
-        cin>>title;
-        cout<<"enter price: ";
-        cin>>price;
-    }
-
-    void display(){
-        cout<<"Title: "<<title<<endl;
-        cout<<"Price: "<<price<<endl;
-    }
+        void getdata(){
+            cout<<"\nEnter Title: ";
+            fflush(stdin);
+            gets(title);
+            cout<<"Enter price: ";
+            cin>>price;
+        }
+        void display(){
+            cout<<"\nDetails"<<endl;
+            cout<<"Title: "<<title<<endl;
+            cout<<"Price: "<<price<<endl;
+        }
 };
 
 class sales{
     protected:
-    float arr_sales[3];
+        float sale[3];
 
-    public:
-    void getdata(){
-        for(int i=0; i<3; i++){
-            cin>>arr_sales[i];
+    public: 
+        void getdata1(){
+            cout<<"Enter sales of last 3 months:\n";
+            for(int i=0;i<3;i++){
+            cin>>sale[i];
+            }
         }
-    }
-
-    void display(){
-        cout<<"sales of a publication for the last three months";
-        for(int j=0; j<3; j++){
-            cout<<arr_sales[j]<<endl;
+        void display1(){
+            cout<<"Sales: "<<endl;
+            for(int i=0;i<3;i++){
+            cout<<sale[i]<<endl;
+            }
         }
-    }
 };
 
-class book:public publication , public sales{
+class book:public publication,public sales{
     protected:
-    long int pages;
+        int pages;
 
     public:
-    void getdata(){
-        cout<<"Enter number of pages: ";
-        cin>>pages;
-    }
+        void getdata2(){
+            cout<<"Enter pages: ";
+            cin>>pages;
+        }
+        void display2(){
+            cout<<"Pages: "<<pages<<endl;
+        }
+};
 
-    void display(){
-        cout<<"Number of Pages: "<<pages<<endl;
-    }
+class tape:public publication,public sales{
+    protected:
+        int rolls;
+        char company[30];
+
+    public:
+        void getdata3(){
+            cout<<"Enter no of rolls: ";
+            cin>>rolls;
+            cout<<"Enter Company: ";
+            fflush(stdin);
+            gets(company);
+        }
+        void display3(){
+            cout<<"No of Rolls: "<<rolls<<endl;
+            cout<<"Company: "<<company<<endl;
+        }
+};
+
+class pamphlet:public publication{
+    protected:
+        int pam_no;
+
+    public: 
+        void getdata4(){
+            cout<<"Enter pamphlet no: ";
+            cin>>pam_no;
+        }
+        void display4(){
+            cout<<"Pamphlet no.: "<<pam_no<<endl;
+        }
+};
+
+class notice:public pamphlet{
+    protected:
+        int notice_no;
+        
+    public:
+        void getdata5(){
+            cout<<"Enter Notice no: ";
+            cin>>notice_no;
+        }
+        void display5(){
+            cout<<"Notice no: "<<notice_no<<endl;
+        }
 };
 
 int main(){
-    book obj;
-    obj.getdata();
-    obj.display();
-
+    book b;
+    tape t;
+    notice n;
+    b.getdata();
+    b.getdata1();
+    b.getdata2();
+    b.display();
+    b.display1();
+    b.display2();
+    t.getdata();
+    t.getdata1();
+    t.getdata3();
+    t.display();
+    t.display1();
+    t.display3();
+    n.getdata();
+    n.getdata4();
+    n.getdata5();
+    n.display();
+    n.display4();
+    n.display5();
 return 0;
 }
